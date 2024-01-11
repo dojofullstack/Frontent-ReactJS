dcb689946b84eae3721942e63c8d39035438e797
dojofullstack <hola@dojofullstack.com>
2024-01-10 19:21:18 -0500

    first commit

---
+++ app-facebook/src/components/Main.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+const Main = (imagen) => {
+    return (
+        <div>
+            <h1>{imagen.titulo}</h1>
+            <img src={imagen.url} />
+        </div>
+    )
+}
+
+
+export default Main;
---
+++ hello-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+        <h1>Hola Dojo</h1>
+      <p>mas informacion</p>
+    </>
+
+  );
+}
+
+export default App;
