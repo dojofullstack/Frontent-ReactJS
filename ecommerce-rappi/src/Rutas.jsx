001c8847e79268f0fe70a5e220c43498490e312e
dojofullstack <hola@dojofullstack.com>
2024-02-07 21:47:00 -0500

    apit auth

--- ecommerce-rappi/src/Rutas.jsx
+++ ecommerce-rappi/src/Rutas.jsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Restaurante from "./views/Restaurante";
 import ProductosMarca from "./views/ProductosMarca";
+import Login from "./views/Login";
+import Checkout from "./views/Checkout";
 
 const Rutas = () => {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/checkout" element={<Checkout />} />
       <Route path="/restaurantes" element={<Restaurante/>} />  
       <Route path="/restaurantes/:marcaID" element={<ProductosMarca/>} />  
       </Routes>
--- ecommerce-rappi/src/components/ModalDetail.jsx
+++ ecommerce-rappi/src/components/ModalDetail.jsx
@@ -1,22 +1,62 @@
+import { useNavigate } from "react-router-dom";
 import useEcommerceStore from "../store";
 
 
 
+
 export const ModalDetail = () => {
-    const productID =  useEcommerceStore ((state) =>  state.productID);
+  const productID =  useEcommerceStore ((state) =>  state.productID);
+  const productDetail =  useEcommerceStore ((state) =>  state.productDetail);
+  const navigate = useNavigate();
+  const isLoginActive =  useEcommerceStore ((state) =>  state.isLoginActive);
 
 
+
+
+  const completarPedido = () => {
+    console.log('isLoginActive', isLoginActive);
+    if (!isLoginActive){
+      navigate('/login');
+    } else {
+      navigate('/checkout');
+    }
+  
+  }
+  
+
+  
 
     return (
         <>
 <dialog id="my_modal_product" className="modal">
   <div className="modal-box w-11/12 max-w-5xl">
-    <h3 className="font-bold text-lg">ProductoID: {productID}!</h3>
-    <p className="py-4">Click the button below to close</p>
+    <h3 className="font-bold text-lg">{productDetail.name}</h3>
+    
+    <div className="flex">
+
+
+      <div className="m-1 p-2">
+          <img src={`https://images.rappi.pe/products/${productDetail.image}`} />
+      </div>
+
+      <div className="m-1 p-2">
+        <p className="text-2xl">
+        ${productDetail.price} USD
+        </p>
+        <p>
+            {productDetail.description}
+        </p>
+      </div>
+
+    </div>
+
+
+
     <div className="modal-action">
       <form method="dialog">
         {/* if there is a button, it will close the modal */}
-        <button className="btn">Close</button>
+        <button className="btn me-5">Cerrar</button>
+        <button onClick={() => completarPedido()} className="btn btn-success">Completar pedido</button>
       </form>
     </div>
   </div>
